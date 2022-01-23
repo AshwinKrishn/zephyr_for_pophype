d9a1cb1b1ef7fa0d59c80b6c131d6e7a1413c43a
AshwinKrishn <ashwinstringed@gmail.com>
2022-01-23 18:22:52 -0500

    Offloading working. Sharedmem is working perfectly

--- ep_x64/src/ep.c
+++ ep_x64/src/ep.c
@@ -54,8 +54,7 @@
 #include "print_results.h"
 #include "device.h"
 #include <zephyr/types.h>
-
-#include <drivers/virtualization/ivshmem.h>
+#include <sys/time.h>
 
 #define MAX(X,Y)  (((X) > (Y)) ? (X) : (Y))
 
@@ -68,9 +67,116 @@
 #define A         1220703125.0
 #define S         271828183.0
 
+#define DISTRIBUTION_ENABLE 1
+
 static double x[2*NK];
 static double q[NQ]; 
 
+enum request_type{
+	COMPUTE_VRANLC,
+	COMPUTE_RANDLC,
+	COMPUTE_LOG,
+	RESPOND_VRANLC
+
+};
+enum arg_type {
+	CHAR,
+	CHAR_P,
+	DOUBLE,
+	DOUBLE_P,
+	INT
+	
+};
+struct argument {
+	uint32_t * location;
+	uint32_t size;
+	enum arg_type type;	
+};
+
+struct offload_struct{
+	uint32_t new_request ;
+	enum request_type type;
+	struct argument args[8];
+};
+
+struct shared_area
+{
+        volatile void * write_area;
+        volatile void * read_area;
+};
+
+struct handshake
+{
+        uint32_t  present;
+        char arch[10];
+};
+volatile struct shared_area rw_buf; 
+
+
+
+void vranlc_dist( int n, double *x, double a, double y[] ){
+
+	printf("Size of double is %d\n",sizeof(double));
+
+	struct offload_struct  ofld_vranlc ;
+	ofld_vranlc.new_request = 0xF00F0FF0;
+	void * write_pointer = (void*)((char*)rw_buf.write_area + 0x1000);
+	
+	*((int*)write_pointer)  = n; 
+	ofld_vranlc.args[0].location =  (uint32_t*)((char*)write_pointer + 0x40000000);
+	printf("Location of the int is %p\n",ofld_vranlc.args[0].location);
+	write_pointer =  (void*)((char*)write_pointer +  sizeof(int));
+	
+	printf("Size after incrementing is %p\n",write_pointer);	
+	*((double*)write_pointer) = *x;
+        ofld_vranlc.args[1].location =  (uint32_t*)((char*)write_pointer + 0x40000000) ;
+	printf("Location of the float pointer is %p\n",ofld_vranlc.args[1].location);
+	write_pointer  =  (void*)((char*)write_pointer +  sizeof(double));
+        
+	*((double*)write_pointer) = a;
+	ofld_vranlc.args[2].location =  (uint32_t*)((char*)write_pointer + 0x40000000) ;
+	write_pointer  =  (void*)((char*)write_pointer +  sizeof(double));
+	
+	*((double*)write_pointer) = *y;	
+        ofld_vranlc.args[3].location = (uint32_t*)((char*)write_pointer + 0x40000000)   ;
+	
+		
+	ofld_vranlc.type = COMPUTE_VRANLC;
+	ofld_vranlc.args[0].size = 1;
+	ofld_vranlc.args[0].type = INT; 
+	ofld_vranlc.args[1].size = 1;
+        ofld_vranlc.args[1].type = DOUBLE;
+	ofld_vranlc.args[2].size = 1;
+        ofld_vranlc.args[2].type = DOUBLE;
+	ofld_vranlc.args[3].size = 1;
+        ofld_vranlc.args[3].type = DOUBLE;
+	
+	memcpy((void*)rw_buf.write_area , (void*)&ofld_vranlc , sizeof(struct offload_struct) );
+	printf("Copied the sturct details\n");
+	printf("Args are %d , %f , %f and %f\n", n,*x,a,*y);	
+	int request_feedback = 1;
+	while(1)
+	{
+		if((*((uint32_t*)rw_buf.write_area) == ~(0xF00F0FF0) ) && request_feedback )
+		{
+			printf("ARM has started calculating vranlc\n");
+			request_feedback = 0;
+		}
+		else
+		{
+			struct offload_struct *  outp = (struct offload_struct *)rw_buf.read_area ;
+			if(outp->new_request == 0xF00F0FF0)
+			{
+				*x = *(double*)outp->args[1].location;
+				*y = *(double*)outp->args[3].location;
+				printf("retrived values from ARM machine \n");
+				break;
+			}
+			
+		}
+
+	}	
+}
 
 int main() 
 {
@@ -82,20 +188,65 @@ int main()
   int    k_offset, j;
   logical verified, timers_enabled;
 
+  printf("x86 kernel started\n");
+  rw_buf.write_area = (void*)0x50000000;
+  rw_buf.read_area  = (void*)0x5f000000;
+  struct handshake hnsk = {
+                .present = 0x1FF1F11F,
+        };
+/*	for(int li = 0 ; li < 20 ; li++)
+	{
+		printf("char at %p is %c\n",((char *)rw_buf.read_area + li) , *((char *)rw_buf.read_area + li));
+	}
+*/	
+        struct handshake in_hnsk;
+        *((uint32_t*)rw_buf.write_area) = 0x00000000 ;
+        memcpy(hnsk.arch, "x86",4);
+        memcpy((void*)&in_hnsk,rw_buf.read_area,sizeof(struct shared_area));
+        if(in_hnsk.present == 0xE00E0EE0)
+        {
+                printf("Already other core %s present in memory.\n",in_hnsk.arch);
+                memcpy(rw_buf.write_area,(void*)&hnsk,sizeof(struct shared_area));
+        }
+	else{
+                printf("Attempting connection with other core ::\n");
+                static uint64_t l = 0;
+                while(1)
+                {
+                        volatile struct handshake * other = (struct handshake*)rw_buf.read_area;
+                        if(other->present == 0xE00E0EE0)
+                        {
+                                printf("Other core of type %s conected \n",other->arch);
+                                break;
+                        }
+                        else
+                        {
+                                if(l % 9000000 == 0)
+                                {
+                                        printf("waiting for other core\n");
+                                        memcpy(rw_buf.write_area,(void*)&hnsk,sizeof(struct shared_area));
+                                }
+                                l++;
+                        }
+                }
+        }
+	*((uint32_t*)rw_buf.read_area) = 0x00000000 ;
+        *((uint32_t*)rw_buf.write_area) = 0x00000000 ;
+	printf("x86_kernel_ shraed memory area exiting\n");
+	struct timeval tv;
+  gettimeofday(&tv,(void *)0);
+  printf("Seconds recorded is %d \n\n",tv.tv_sec);
   double dum[3] = {1.0, 1.0, 1.0};
   char   size[16];
-  void * mallocing_space ;
   FILE *fp;
-  char arr[] = "Ashwin";
-  struct device * ivshmem_device = 0x00000000; 
-  mallocing_space = malloc(1000);
   if ((fp = fopen("timer.flag", "r")) == NULL) {
     timers_enabled = false;
   } else {
     timers_enabled = true;
     fclose(fp);
   }
-
+  
+  
   //--------------------------------------------------------------------
   //  Because the size of the problem is too large to store in a 32-bit
   //  integer for some classes, we put it into a string (for printing).
@@ -126,8 +277,17 @@ int main()
   //  Also, call all mathematical functions that are used. Make
   //  sure these initializations cannot be eliminated as dead code.
   //--------------------------------------------------------------------
-/*
-  vranlc(0, &dum[0], dum[1], &dum[2]);
+  if(DISTRIBUTION_ENABLE == 1)
+  {
+	
+	vranlc_dist(10000, &dum[0], dum[1], &dum[2]);	
+	
+  }
+  else{
+	printf("before vranlc dum[2] value is %f",dum[2]);
+  	vranlc(10, &dum[0], dum[1], &dum[2]);
+	printf("With DISTRIBUTION_ENABLE 0 and n 10, dum[2] value is %f and dum[0] value is %f\n",dum[2],dum[0]);
+  }
   dum[0] = randlc(&dum[1], dum[2]);
   for (i = 0; i < 2 * NK; i++) {
     x[i] = -1.0e99;
@@ -142,11 +302,11 @@ int main()
 
   t1 = A;
   vranlc(0, &t1, A, x);
-*/
+
   //--------------------------------------------------------------------
   //  Compute AN = A ^ (2 * NK) (mod 2^46).
   //--------------------------------------------------------------------
-/*
+
   t1 = A;
 
   for (i = 0; i < MK + 1; i++) {
@@ -162,13 +322,13 @@ int main()
   for (i = 0; i < NQ; i++) {
     q[i] = 0.0;
   }
-*/
+
   //--------------------------------------------------------------------
   //  Each instance of this loop may be performed independently. We compute
   //  the k offsets separately to take into account the fact that some nodes
   //  have more numbers to generate than others
   //--------------------------------------------------------------------
-/*
+
   k_offset = -1;
 
   for (k = 1; k <= np; k++) {
@@ -185,7 +345,7 @@ int main()
       t3 = randlc(&t2, t2);
       kk = ik;
     }
-*/
+
     //--------------------------------------------------------------------
     //  Compute uniform pseudorandom numbers.
     //--------------------------------------------------------------------
@@ -198,7 +358,7 @@ int main()
     //  tally counts in concentri//square annuli.  This loop is not 
     //  vectorizable. 
     //--------------------------------------------------------------------
- /*   if (timers_enabled) timer_start(1);
+    if (timers_enabled) timer_start(1);
 
     for (i = 0; i < NK; i++) {
       x1 = 2.0 * x[2*i] - 1.0;
@@ -224,11 +384,12 @@ int main()
 
 
   timer_stop(0);
-  tm = timer_read(0);
+  tm =timer_read(0);
 
   nit = 0;
   verified = true;
   if (M == 24) {
+    printf("M value is 24\n");
     sx_verify_value = -3.247834652034740e+3;
     sy_verify_value = -6.958407078382297e+3;
   } else if (M == 25) {
@@ -270,13 +431,15 @@ int main()
   for (i = 0; i < NQ; i++) {
     printf("%3d%15.0lf\n", i, q[i]);
   }
-*/
+
   print_results("EP", CLASS, M+1, 0, 0, nit,
       tm, Mops, 
       "Random numbers generated",
       verified, NPBVERSION, COMPILETIME, CS1,
       CS2, CS3, CS4, CS5, CS6, CS7);
 
+  gettimeofday(&tv,(void *)0);
+  printf("Seconds recorded is %d \n",tv.tv_sec);
   if (timers_enabled) {
     if (tm <= 0.0) tm = 1.0;
     tt = timer_read(0);
@@ -286,23 +449,6 @@ int main()
     tt = timer_read(2);
     printf("Random numbers: %9.3lf (%6.2lf)\n", tt, tt*100.0/tm);
   }
- ivshmem_device = device_get_binding(CONFIG_IVSHMEM_DEV_NAME);
- printf("ivshmem_device value is %x\n",ivshmem_device);
- 
- void * shm_addr ;
- uint64_t sizeof_shrmem = ivshmem_get_mem(ivshmem_device,&shm_addr); 
- printf("Address of the pointer is %x\n",shm_addr);
- uint32_t * shared_area = (uint32_t*) shm_addr;
- printf("Writing data into shm\n\n");
 
- for(int i = 0 ; i < 100 ; i++)
- {
-	*((char*)shared_area + i) = 'a';
- }
- printf("We are printing the contents of first 100 bytes of ivshmem:\n");
- for(int i = 0 ; i < 100 ; i++)
- {
-	printf("%c",*((char*)shared_area + i));
- }
   return 0;
 }
--- ivshmem_arm64/src/app.c
+++ ivshmem_arm64/src/app.c
@@ -1,6 +1,34 @@
 #include <zephyr.h>
 #include <drivers/virtualization/ivshmem.h>
 #include <stdio.h>
+enum request_type{
+        COMPUTE_VRANLC,
+        COMPUTE_RANDLC,
+        COMPUTE_LOG,
+	RESPOND_VRANLC
+
+};
+enum arg_type {
+        CHAR,
+        CHAR_P,
+        DOUBLE,
+        DOUBLE_P,
+	INT
+
+};
+struct argument {
+        uint32_t * location;
+        uint32_t size;
+        enum arg_type type;
+};
+
+
+
+struct offload_struct{
+        uint32_t new_request ;
+	enum request_type type;
+        struct argument args[8];
+};
 struct handshake
 {
         uint32_t present;
@@ -11,6 +39,57 @@ struct shared_area
         volatile void * write_area;
         volatile void * read_area;
 };
+  typedef struct {
+      unsigned int mantissa_low:32;     
+      unsigned int mantissa_high:20;
+      unsigned int exponent:11;        
+      unsigned int sign:1;
+    } tDoubleStruct;
+
+
+void vranlc( int n, double *x, double a, double y[] )
+{
+  const double r23 = 1.1920928955078125e-07;
+  const double r46 = r23 * r23;
+  const double t23 = 8.388608e+06;
+  const double t46 = t23 * t23;
+
+  double t1, t2, t3, t4, a1, a2, x1, x2, z;
+
+  int i;
+
+  //--------------------------------------------------------------------
+  //  Break A into two parts such that A = 2^23 * A1 + A2.
+  //--------------------------------------------------------------------
+  t1 = r23 * a;
+  a1 = (int) t1;
+  a2 = a - t23 * a1;
+
+  //--------------------------------------------------------------------
+  //  Generate N results.   This loop is not vectorizable.
+  //--------------------------------------------------------------------
+  for ( i = 0; i < n; i++ ) {
+    //--------------------------------------------------------------------
+    //  Break X into two parts such that X = 2^23 * X1 + X2, compute
+    //  Z = A1 * X2 + A2 * X1  (mod 2^23), and then
+    //  X = 2^23 * Z + A2 * X2  (mod 2^46).
+    //--------------------------------------------------------------------
+    t1 = r23 * (*x);
+    x1 = (int) t1;
+    x2 = *x - t23 * x1;
+    t1 = a1 * x2 + a2 * x1;
+    t2 = (int) (r23 * t1);
+    z = t1 - t23 * t2;
+    t3 = t23 * z + a2 * x2;
+    t4 = (int) (r46 * t3) ;
+    *x = t3 - t46 * t4;
+    y[1] = r46 * (*x);
+  }
+
+  return;
+}
+
+
 void main()
 {
 	printf("arm64 app started\n");
@@ -51,8 +130,60 @@ void main()
 			}
                 }
         }
+	while(1)
+	{
+		struct offload_struct *  inp = (struct offload_struct *)rw_buf.read_area ;
+		if(inp->new_request == 0xF00F0FF0)
+		{
+			printf("A request of type %d came\n",COMPUTE_VRANLC);
+			printf("Args are %d , %f , %f and %f\n", *inp->args[0].location,*(double*)inp->args[1].location,*(double*)inp->args[2].location,*(double*)inp->args[3].location);
+			vranlc(*(int*)inp->args[0].location,(double*)inp->args[1].location,*(double*)inp->args[2].location , (double*)inp->args[3].location);
+			printf("Post computation value of y is %f and value of x is %f\n",*((double*)inp->args[3].location), *((double*)inp->args[1].location));			
+			
+			//indicate that you have consumed the message
+			*(uint32_t*)inp = ~(0xF00F0FF0);
+
+			//Start replying
+			struct offload_struct ofld_vranlc ;
+
+			ofld_vranlc.new_request = 0xF00F0FF0;
+			void * write_pointer = (void*)((char*)rw_buf.write_area + 0x1000);
+			
+			*((int*)write_pointer)  = *(int*)inp->args[0].location;
+		        ofld_vranlc.args[0].location =  (uint32_t*)((char*)write_pointer - 0x40000000);
+        		printf("Location of the int is %p\n",ofld_vranlc.args[0].location);
+      			write_pointer =  (void*)((char*)write_pointer +  sizeof(int));
+
+        		*((double*)write_pointer) = *(int*)inp->args[1].location;
+        		ofld_vranlc.args[1].location =  (uint32_t*)((char*)write_pointer - 0x40000000) ;
+        		printf("Location of the float pointer is %p\n",ofld_vranlc.args[1].location);
+        		write_pointer  =  (void*)((char*)write_pointer +  sizeof(double));
+
+        		*((double*)write_pointer) = *(int*)inp->args[2].location;
+        		ofld_vranlc.args[2].location =  (uint32_t*)((char*)write_pointer - 0x40000000) ;
+        		write_pointer  =  (void*)((char*)write_pointer +  sizeof(double));
+
+        		*((double*)write_pointer) = *(int*)inp->args[3].location;
+        		ofld_vranlc.args[3].location = (uint32_t*)((char*)write_pointer - 0x40000000)   ;
+
+
+        		ofld_vranlc.type = COMPUTE_VRANLC;
+        		ofld_vranlc.args[0].size = 1;
+        		ofld_vranlc.args[0].type = INT;
+        		ofld_vranlc.args[1].size = 1;
+			ofld_vranlc.args[1].type = DOUBLE;
+        		ofld_vranlc.args[2].size = 1;
+        		ofld_vranlc.args[2].type = DOUBLE;
+        		ofld_vranlc.args[3].size = 1;
+        		ofld_vranlc.args[3].type = DOUBLE;
+
+        		memcpy((void*)rw_buf.write_area , (void*)&ofld_vranlc , sizeof(struct offload_struct) );				
+
+			break;	
+		}
+	}
+	printf("ARM_kernel_exiting\n");
      	*((uint32_t*)rw_buf.read_area) = 0x00000000 ;
      	*((uint32_t*)rw_buf.write_area) = 0x00000000 ;
-	printf("ARM_kernel_exiting\n");
 	while(1);
 }
